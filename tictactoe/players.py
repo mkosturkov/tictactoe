67ee042d638abaf649ca760754976e09fc4ba1bb
Milko Kosturkov <mkosturkov@gmail.com>
2017-03-12 11:34:21 +0200

    Add AI player

--- tictactoe/players.py
+++ tictactoe/players.py
@@ -1,3 +1,5 @@
+import ai
+import random
 
 
 class Player:
@@ -14,3 +16,10 @@ class ConsolePlayer(Player):
         xy = raw_input('Player %s, Enter a move (xy): ' % self.mark)
         x, y = int(xy[0]), int(xy[1])
         board.mark_position(x, y, self)
+        
+class AIPlayer(Player):
+    
+    def make_move(self, board):
+        scores = ai.get_best_moves_for_player(self, board)
+        selected_score = scores[random.randint(0, len(scores) - 1)]
+        selected_score.position.mark_player(self)
--- ttt.py
+++ ttt.py
@@ -1,10 +1,10 @@
 from tictactoe.board import Board
-from tictactoe.players import ConsolePlayer
+from tictactoe.players import ConsolePlayer, AIPlayer
 from tictactoe.outputs import ConsoleOutput
 from tictactoe.game import Game
 
-player1 = ConsolePlayer(mark='x')
-player2 = ConsolePlayer(mark='o')
+player1 = AIPlayer(mark='x')
+player2 = AIPlayer(mark='o')
 board = Board(3)
 game = Game(player1, player2, board)
 output = ConsoleOutput()
