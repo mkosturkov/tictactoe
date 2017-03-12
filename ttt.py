d2bbaafe27b2cb260dfd8e6d85e4a61daee16f84
Milko Kosturkov <mkosturkov@gmail.com>
2017-03-12 12:21:19 +0200

    Allow player to chose game mode

--- ttt.py
+++ ttt.py
@@ -3,8 +3,15 @@ from tictactoe.players import ConsolePlayer, AIPlayer
 from tictactoe.outputs import ConsoleOutput
 from tictactoe.game import Game
 
-player1 = AIPlayer(mark='x')
-player2 = AIPlayer(mark='o')
+number_of_players = int(raw_input('How many players? '))
+if number_of_players == 1:
+    play_as_player = int(raw_input('Play as player? '))
+else:
+    play_as_player = None
+
+
+player1 = AIPlayer(mark='x') if number_of_players == 0 or (number_of_players == 1 and play_as_player == 2) else ConsolePlayer(mark='x')
+player2 = AIPlayer(mark='o') if number_of_players == 0 or (number_of_players == 1 and play_as_player == 1) else ConsolePlayer(mark='o')
 board = Board(3)
 game = Game(player1, player2, board)
 output = ConsoleOutput()
